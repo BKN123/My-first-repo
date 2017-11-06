9b5ea5ab8716485cb3f379a2cb18c235905521f6
BKN123 <33422428+BKN123@users.noreply.github.com>
2017-11-06 08:39:44 -0500

    Initial Commit

---
+++ Mad Lib_Blaise.py
@@ -0,0 +1,25 @@
+print("Write a noun")
+noun1 = input()
+
+print("Write another noun")
+noun2 = input()
+
+print("Write an adjective")
+adjective1 = input()
+
+print("Write a verb")
+verb1 = input()
+
+
+### MAD LIB ###
+
+print("Meet our hero" + Captainjelly1 + ", + a super-intelligent"
+      + uprof1 + ".A run-in with a" + country1 +
+      " military leads him to become evil"
+      + Captainjelly2 + ", a"
+       +color1 + "" + adjective1 + "giant capable of great desctruction. He"
+      +adverb1 + "battles military with his dog"
+      +Captainjelly3 + ". Eventually it is discovered that our hero's long time"
+      "freind" + Jellyfam4 + ", disntinguished by his"
+      + facialfeature1 + "is trying to turn" Jellyfam 4 
+
